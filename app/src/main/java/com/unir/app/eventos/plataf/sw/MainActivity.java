3936666c68bec1c1acf36bebb1564079739532b1
Edison Gudiño <egudino@montran.com>
2021-02-19 13:49:25 -0500

    Initial Commit

---
+++ app/src/main/java/com/unir/app/eventos/plataf/sw/MainActivity.java
@@ -0,0 +1,14 @@
+package com.unir.app.eventos.plataf.sw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
