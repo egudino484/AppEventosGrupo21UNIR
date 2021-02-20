bdde4cfa95419a742ad6992bff78927f8be515a5
Mario Correa <mario.correa@modinter.com.ec>
2021-02-20 09:10:36 -0500

    feat: añadir TimePickerDialog

--- app/src/main/java/com/unir/app/eventos/plataf/sw/MainActivity.java
+++ app/src/main/java/com/unir/app/eventos/plataf/sw/MainActivity.java
@@ -3,11 +3,13 @@ package com.unir.app.eventos.plataf.sw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 import java.util.Locale;
@@ -17,8 +19,11 @@ public class MainActivity extends AppCompatActivity {
     Spinner spinnerEvent;
     EditText dateInit;
     EditText dateEnd;
+    EditText hourInit;
+    EditText hourEnd;
 
     private int lastYear, lastMonth, lastDayMonthYear;
+    private int lastHours, lastMinutes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +36,19 @@ public class MainActivity extends AppCompatActivity {
 
         dateInit = findViewById(R.id.dateInit);
         dateEnd = findViewById(R.id.dateEnd);
+        hourInit = findViewById(R.id.hourInit);
+        hourEnd = findViewById(R.id.hourEnd);
         final Calendar calendar = Calendar.getInstance();
         lastYear = calendar.get(Calendar.YEAR);
         lastMonth = calendar.get(Calendar.MONTH);
         lastDayMonthYear = calendar.get(Calendar.DAY_OF_MONTH);
+        lastHours = calendar.get(Calendar.HOUR_OF_DAY);
+        lastMinutes = calendar.get(Calendar.MINUTE);
 
         refreshDateText(dateInit);
         refreshDateText(dateEnd);
+        refreshHourText(hourInit);
+        refreshHourText(hourEnd);
 
         dateInit.setOnClickListener(v -> {
             DatePickerDialog dialogDateInit = new DatePickerDialog(MainActivity.this, listenerInitDatePicker, lastYear, lastMonth, lastDayMonthYear);
@@ -49,8 +60,35 @@ public class MainActivity extends AppCompatActivity {
             dialogDateEnd.show();
         });
 
+        hourInit.setOnClickListener(v -> {
+            TimePickerDialog dialogHourInit = new TimePickerDialog(MainActivity.this, onTimeSetListenerInit, lastHours, lastMinutes, false);
+            dialogHourInit.show();
+        });
+
+        hourEnd.setOnClickListener(v -> {
+            TimePickerDialog dialogHourEnd = new TimePickerDialog(MainActivity.this, onTimeSetListenerEnd, lastHours, lastMinutes, false);
+            dialogHourEnd.show();
+        });
 
     }
+
+    private final TimePickerDialog.OnTimeSetListener onTimeSetListenerInit = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int hours, int minutes) {
+            lastHours = hours;
+            lastMinutes = minutes;
+            refreshHourText(hourInit);
+        }
+    };
+
+    private final TimePickerDialog.OnTimeSetListener onTimeSetListenerEnd = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int hours, int minutes) {
+            lastHours = hours;
+            lastMinutes = minutes;
+            refreshHourText(hourEnd);
+        }
+    };
 
     private final DatePickerDialog.OnDateSetListener listenerInitDatePicker = new DatePickerDialog.OnDateSetListener() {
         @Override
@@ -77,6 +115,11 @@ public class MainActivity extends AppCompatActivity {
     public void refreshDateText(EditText editText) {
         String date = String.format(Locale.getDefault(), "%02d-%02d-%02d", lastYear, lastMonth+1, lastDayMonthYear);
         editText.setText(date);
+    }
+
+    public void refreshHourText(EditText editText) {
+        String hours = String.format(Locale.getDefault(), "%02d:%02d", lastHours, lastMinutes);
+        editText.setText(hours);
     }
 
 }
